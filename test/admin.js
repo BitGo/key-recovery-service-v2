43ca31eb18b2f9ae6b7a584917eb3f1962a4bac3
David Cruz <davidcruz@bitgo.com>
2018-07-25 16:56:39 -0700

    add note on file import testing

--- test/admin.js
+++ test/admin.js
@@ -4,6 +4,11 @@ const admin = require('../bin/admin.js');
 
 describe('Offline Admin Tool', function() {
   describe('Xpub validation', function() {
+    // Xpub importing was tested with a local file with contents: (w/o line breaks)
+    // xpub6AHA9hZDN11k2ijHMeS5QqHx2KP9aMBRhTDqANMnwVtdyw2TDYRmF8PjpvwUFcL1Et8Hj59S3gTSMcUQ5gAqTz3Wd8EsMTmF3DChhqPQBnU,
+    // xpub69pXXVvsBtZHWE1wgoix7xRAnbp1r6tR1kTK9cKvCd4QgYh4JBSLBmLA65Kg7rCMwGYrNHKFKxZDtjRkU4Ex2ozMYGfk14EyotJ5xjf2Goy,
+    // xpub6AU2iYgKUY8FvUc2Nz2fcWpKU1HJTeYTbzv4MZLGPBw2YnhoZPkkUo54fvqZyVtxtszMdyksF8k3iqMcoegyvj72xKZCmuCjDWneXjjztLN
+    // and successfully saved to local Mongo instance
     describe('failure', function() {
       it('should fail if length is not 111', function() {
         const SHORT_XPUB = 'xpub1234567890';
